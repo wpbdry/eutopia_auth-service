759d447d2b996c19d56b520f1efb3465178f9acd
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-03-03 20:13:19 +0100

    [EUT-7] (feat): add db setup script

---
+++ db_setup.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA auth;
+
+CREATE TABLE auth.user (
+  uid         char(20)      UNIQUE NOT NULL,
+  full_name   varchar(70)   NOT NULL,
+  call_name   varchar(20),
+  email       varchar(70)   UNIQUE NOT NULL,
+  password    varchar(50)   NOT NULL
+);
+
+INSERT INTO auth.user (full_name, call_name, email, password, uid)
+  VALUES ('Santa Claus', 'Santa', 'santa@claus.example', 'very_secure', 'abcdefghijklmnopqrst');
